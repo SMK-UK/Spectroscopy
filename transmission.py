d6778f769948a22f9aa6205e9919e00d5bc972cd
Sean Keenan <keenan.sean@hotmail.co.uk>
2022-11-09 19:32:49 +0000

    Add .py files and remove surplus

--- transmission.py
+++ transmission.py
@@ -1,22 +1,37 @@
+'''
+Spectroscopic Data Analysis
+Sean Keenan, PhD Physics
+Quantum Memories Group, Heriot-Watt University, Edinburgh
+2021
+'''
+
 import os, re
 import numpy as np
+import numpy.ma as ma
 import matplotlib.pyplot as mp
-import scipy.interpolate as sp
+import scipy.interpolate as si
+import scipy.signal as ss
 import spec_funcs as sf
-from scipy.fftpack import fft, fftfreq
-from natsort import natsorted
+from scipy.fftpack import rfft, irfft, fftfreq
+from statistics import mean
 
 # change to gui at later date
 # polarisations as written in file names
 polarisations = ["143", "188"]
 
 # folder containing all the requisite subfolders and data - refrences included
-path = "C:\\Users\\keena\\Desktop\\New Folder"
+path = "C:\\Users\\sk88\\Desktop\\New Folder"
 
 # choose to focus on a particular wavelength range
 zoom = 1
-lower = 590
-upper = 610
+lower = 550
+upper = 650
+
+# used to convert raw data to nm (future)
+converter = 1
+
+# save images (1 for save)
+save_fig = 0
 
 # initialise lists
 file_list = []
@@ -93,16 +108,30 @@ for polarisation in polarisations:
 interpolate reference data for each file - avoids issue of wavelength array length mismatch
 
 '''
+check_temp = []
 # cycle through polarisations
 for idx_1 in range(len(polarisations)):
     temp_I_0 = []
     temp_OD = []
+    I_temp = []
     # generate spline co-effs
-    spline_coeffs = sp.splrep(bg_wavelengths[idx_1], bg_Is[idx_1])
+    spline_coeffs = si.splrep(bg_wavelengths[idx_1], bg_Is[idx_1])
     # calculate referenece for each wavelength subset
     for idx_2, wavelength in enumerate(wave_sets[idx_1]):
-        temp_I_0.append(sp.splev(wavelength, spline_coeffs))
-        temp_OD.append(np.log10(np.abs(temp_I_0[idx_2]/I_sets[idx_1][idx_2])))
+        spline_temp = (si.splev(wavelength, spline_coeffs))
+        I_temp = I_sets[idx_1][idx_2]
+        # scale data to remove negative values
+        if np.nanmin(spline_temp) < 0 or np.nanmin(I_temp) < 0:
+            if np.nanmin(spline_temp) < np.nanmin(I_temp):
+                adjust = np.nanmin(spline_temp)
+            elif np.nanmin(I_temp) < np.nanmin(spline_temp):
+                adjust = np.nanmin(I_temp)
+            temp_I_0.append(spline_temp - adjust)
+            I_temp -= adjust
+        else:
+            temp_I_0.append(spline_temp)
+        temp_OD.append(np.log(13*temp_I_0[idx_2]/I_temp))
+    check_temp.append(I_temp)
     I_0.append(temp_I_0)
     OD.append(temp_OD)
 
@@ -131,9 +160,17 @@ elif zoom != 1:
         starts.append(start)
         stops.append(stop)
 
-# normaise the OD
-OD_norm = OD / np.nanmax(OD)
+# mask OD data to remove -inf, nan and inf from normalisation
+masked_OD = ma.masked_invalid(OD)
+# normalise the OD
+OD_norm = masked_OD / np.nanmax(masked_OD)
 
+if zoom == 0:
+    string_format = path + '\\' + str(round(min(bg_wavelengths[0]))) + '_' + str(round(max(bg_wavelengths[0]))) + '_'
+else:
+    string_format = path + '\\' + str(lower) + '_' + str(upper) + '_'
+
+# create separate plots for each polarisation
 for idx_1, polarisation in enumerate(polarisations):
     
     fig_1, ax_1 = mp.subplots(figsize=(8, 5))
@@ -144,13 +181,17 @@ for idx_1, polarisation in enumerate(polarisations):
     ax_1.grid(True)
     ax_1.grid(True, color='silver', linewidth=0.5)
 
+    
+    #shift each spectrum by a defined value
     shift = 0
-
     for idx_2, wave in enumerate(wave_sets[index]):
         lbl = os.path.split(folder_list[idx_2])
         data = OD_norm[idx_1][idx_2] + shift
-        ax_1.plot(wave[start[idx_2]:stop[idx_2]], data[start[idx_2]:stop[idx_2]], color=None, marker=None, linestyle='-', alpha=0.8, label=lbl[1])
-        
-        shift += 0.05
+        ax_1.plot(wave[start[idx_2]:stop[idx_2]], data[start[idx_2]:stop[idx_2]], color=None, marker=None, linestyle='-', alpha=1, label=lbl[1])
+
+        shift += (0.25)
     
-    ax_1.legend(loc='best', fontsize=8)
+    ax_1.legend(loc='upper left', fontsize=8)
+
+    if save_fig == 1:
+        fig_1.savefig(fname=string_format + str(polarisation) + '.jpg', dpi='figure', format='jpg', bbox_inches='tight')
