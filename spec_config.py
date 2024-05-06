5c6399221f2646c8b94bc55a4db2f6303f555ef8
SMK-UK <keenan.sean@hotmail.co.uk>
2024-05-06 01:08:47 +0100

    General update

--- spec_config.py
+++ spec_config.py
@@ -10,18 +10,22 @@ exceptions          = ["notes", "setup", "ignore", "Thermal OD"]
 extensions          = ['.csv', '.txt', '.CSV']
 # reference names
 refs                = ["ref", "reference", "R"]
-# calibrate wavelength of OSA
+# calibrate wavelength of OSA (nm)
 calibration = 1.054269203603
 # reference power correction
 correction = 345/50
 # averaging window length
 window = 50
 
-config_params = dict(
+# Wavelengths of interest
 
-    # Index positions for relevant column data in excel files
-    
-    data_indexes = dict(
+woi = dict(
+
+    a = 1,
+
+),
+
+data_indexes = dict(
 
     time = 0,
     sp_trans = 1,
@@ -29,23 +33,6 @@ config_params = dict(
     cp_trans = 3,
     cp_ref = 4
 
-    ),
-
-    # Index positions for trimming the data
-
-    trim_indexes = dict(
-    
-    trig                = 50026, 
-    ref_off             = 300,
-    off                 = 1100, 
-    ramp                = 65026,
-
-    ),
-
-    # Guess T1 times for the data
-
-    guess_ref_T1 = 1E-6,
-    guess_T1 = 10E-6
-
 )
+
 
