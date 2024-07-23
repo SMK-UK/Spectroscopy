453ba25ead29b57a2f02c61cc1c8df95af8fd16f
SMK-UK <keenan.sean@hotmail.co.uk>
2024-07-23 12:06:25 +0100

    update

---
+++ PrYVO/PrYVO_config.py
@@ -0,0 +1,36 @@
+"""
+Config file for PrYVO IR measurements
+"""
+
+# Path to load data from and arguments used to disriminate files loaded
+root                = "C:/Users/sk88/Desktop"
+file                = "0725_BB_IR_COL_T"
+# file extension types
+exceptions          = ["notes", "setup", "ignore", "Thermal OD"]
+extensions          = ['.csv', '.txt', '.CSV']
+# reference names
+refs                = ["ref", "reference", "R"]
+# calibrate wavelength of OSA (nm)
+calibration = 1.054269203603
+# reference power correction
+correction = 345/50
+# averaging window length
+window = 50
+
+# Wavelengths of interest
+
+woi = dict(
+
+    a = 1,
+
+),
+
+data_indexes = dict(
+
+    time = 0,
+    sp_trans = 1,
+    sp_ref = 2,
+    cp_trans = 3,
+    cp_ref = 4
+
+)
