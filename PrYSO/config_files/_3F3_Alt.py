4f5fd4281b73ae748c22954fd08437787dade1ad
Sean Keenan <keenan.sean@hotmail.co.uk>
2024-10-07 16:40:01 +0300

    add PrYSO analysis folder. Remake venv and add requirements.txt

---
+++ PrYSO/config_files/_3F3_Alt.py
@@ -0,0 +1,26 @@
+"""
+Config file for PrYVO IR measurements
+"""
+
+# Path to load data from and arguments used to disriminate files loaded
+root                = r"C:\Users\sk88\Heriot-Watt University Team Dropbox\RES_EPS_Quantum_Photonics_Lab\Experiments\Current Experiments\BB Telecom QM\2024_PrYSO\Spectroscopy\Teledyne\03_2036_17\1909\WL"
+file                = r"IR_1"
+# file extension types
+exceptions          = ["notes", "setup", "ignore", "Thermal OD"]
+extensions          = ['.csv', '.txt', '.CSV']
+# reference names
+refs                = ["ref", "reference", "R"]
+# polarisations
+polarisations = ['FAV', 'SAV']
+# calibrate wavelength of OSA (nm)
+calibration = 1
+# reference power correction
+correction = 1
+# averaging window length
+window = 50
+# single temp file?
+single = False
+# numpy arrays used?
+numpy = False
+
+
